d5e82a15da8365a2c72e6112eb053a888bf2fb6c
ElanurAltintop <altintopelanur01@gmail.com>
2022-08-22 13:16:23 +0300

    AngularProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularProje12Form2';
+
+
+  UserForma=new FormGroup(
+    {
+      userName:new FormControl('', [Validators.required, Validators.minLength(5)]),
+      password:new FormControl('',Validators.required)
+    }
+  );
+
+
+  GetLoginUser()
+  {
+    console.log(this.UserForma.value);
+  }
 }
